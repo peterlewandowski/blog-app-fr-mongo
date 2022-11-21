d92a4671ea05e2eba1a8d204689e95d1e8b9c09e
Peter Lewandowski <mapiotr2010@gmail.com>
2022-11-21 12:03:37 -0500

    react router dom and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './assets/styles.css'
+import AddPost from "./pages/AddPost";
+import Home from "./pages/Home";
+import SinglePost from "./pages/SinglePost";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>my app</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/single-post' element={<SinglePost />} />
+        <Route path='/add-post' element={<AddPost />} />
+        <Route path='*' element={<h2> Page not found</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
